64c6ac3f4aaa9f7d7ceea7ccb40d91f24974d3c0
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-15 16:12:18 +0200

    revert last commit

--- textgenrnn/train.py
+++ textgenrnn/train.py
@@ -1,4 +1,4 @@
 from textgenrnn import textgenrnn
 textgen = textgenrnn()
 
-textgen.train_from_file('/datasets/montagtext.txt', num_epochs=100)
+textgen.train_from_file('montagtext.txt', num_epochs=100)
