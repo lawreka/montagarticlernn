fd1a7371972b8eac37e7594e7be1a86445784dbf
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-16 09:38:48 +0200

    updated script to run training again

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import gpt_2_simple as gpt2
 
-gpt2.download_gpt2()   # model is saved into current directory under /models/117M/
+model_name = "117M"
+gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/117M/
 
 sess = gpt2.start_tf_sess()
-gpt2.finetune(sess, 'montagtext.txt', steps=100)   # steps is max number of training steps
+gpt2.finetune(sess,
+              'montagtext.txt',
+              model_name=model_name,
+              steps=100)   # steps is max number of training steps
 
 gpt2.generate(sess)
