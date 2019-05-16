2952a96f830accfa54b52079967102da9aa369f4
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-16 19:15:25 +0200

    try to point to model from run 181

--- generate.py
+++ generate.py
@@ -1,5 +1,4 @@
 import gpt_2_simple as gpt2
 
-model = "montagmodel"
 sess = gpt2.start_tf_sess()
-gpt2.generate_to_file(sess, destination_path='montagbyai.txt', prefix="The future is ", model_name=model, temperature=1.0)
+gpt2.generate_to_file(sess, destination_path='montagbyai.txt', prefix="The future is ", model_name="montagmodel", temperature=1.0)
