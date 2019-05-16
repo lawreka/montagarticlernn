e45cd54945fe7b8b8b82e1f3e5b21e9f68013450
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-16 20:06:19 +0200

    point to run 181

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import gpt_2_simple as gpt2
 
 sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, run_name=montagmodel)
+gpt2.load_gpt2(sess, run_name="runs/181/")
 
 gpt2.generate_to_file(sess, destination_path='montagbyai.txt', prefix="The future is ", model_name=model_name, temperature=1.0)
