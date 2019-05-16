3cda463f3b3af0de80ee96ec963561a06d923b94
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-16 21:03:05 +0200

    try adding path to run 181 to load

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import gpt_2_simple as gpt2
 
 sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess)
+gpt2.load_gpt2(sess, run_name="spell/runs/181/checkpoint/run1")
 
 gpt2.generate_to_file(sess, destination_path='montagbyai.txt', prefix="The future is ", model_name="spell/runs/181/models/117M", temperature=1.0, run_name="spell/runs/181/checkpoint/run1")
