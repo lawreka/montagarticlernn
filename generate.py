f760e52c5ce80fde2eaae20e880993f77a236303
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-16 20:52:34 +0200

    fixed bad parentheses in generate.py again

--- generate.py
+++ generate.py
@@ -3,4 +3,4 @@ import gpt_2_simple as gpt2
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
 
-gpt2.generate_to_file(sess, destination_path='montagbyai.txt'), prefix="The future is ", temperature=1.0)
+gpt2.generate_to_file(sess, destination_path='montagbyai.txt', prefix="The future is ", temperature=1.0)
