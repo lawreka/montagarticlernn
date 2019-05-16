9f4c730ac4f8291ec8b69d3ecae18bce94dd6422
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-16 19:22:35 +0200

    attempt to load run 181

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import gpt_2_simple as gpt2
 
 sess = gpt2.start_tf_sess()
-model_name = "spell/runs/181/models/117M/"
+gpt2.load_gpt2(sess, run_name="181")
 gpt2.generate_to_file(sess, destination_path='montagbyai.txt', prefix="The future is ", model_name=model_name, temperature=1.0)
