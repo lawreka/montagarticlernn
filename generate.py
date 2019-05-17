18e0010c854c2f8915ed36dd0859d50954350b92
Kathryn Lawrence <kathryn@Kathryns-MacBook-Pro.local>
2019-05-17 15:49:07 +0200

    added prefix, changed temperature, create 20 samples

--- generate.py
+++ generate.py
@@ -3,4 +3,4 @@ import gpt_2_simple as gpt2
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess, run_name="montagmodel")
 
-gpt2.generate(sess, run_name="montagmodel")
+gpt2.generate(sess, run_name="montagmodel", prefix="The future is ", temperature=0.5, nsamples=20)
